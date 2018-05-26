2dcea83c25f72e11b3d673cd0727b4078aa997b9
jbrow78 <brownjudah8@gmail.com>
2018-05-26 14:05:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-function getpage() {
- var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-            var myObj = JSON.parse(this.responseText);
-            document.getElementById("maindis").src=myObj;
-          }
-        };
-        xmlhttp.open("GET","/website.redirect/" + document.getElementById("urlbar").value +".json", true);
-        xmlhttp.send();
-      }
-
+function geturl() {
+var xmlhttp = new XMLHttpRequest();
+       xmlhttp.onreadystatechange = function() {
+         if (this.readyState == 4 && this.status == 200) {
+           var myObj = JSON.parse(this.responseText);
+           document.getElementById("iframe").src=myObj;
+         }
+       };
+       xmlhttp.open("GET","https://jftp.github.io/" + document.getElementById("urlbar").value +".json", true);
+       xmlhttp.send();
+     }
