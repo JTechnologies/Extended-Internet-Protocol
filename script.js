1a42189cf2e7579580589f556872b6daa3455cb7
Velocity <me@jtechnologies.xyz>
2020-05-19 01:15:59 +0000

    Update API

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function geturl() {
-var xmlhttp = new XMLHttpRequest();
-       xmlhttp.onreadystatechange = function() {
-         if (this.readyState == 4 && this.status == 200) {
-           var myObj = JSON.parse(this.responseText);
-           document.getElementById("iframe").src=myObj;
-         }
-       };
-       xmlhttp.open("GET","https://jftp.github.io/website.redirect/" + document.getElementById("urlbar").value +".json", true);
-       xmlhttp.send();
-     }
+  var dns = document.getElementById("dns");
+  var url = document.getElementById("urlbar").value;
+  var frame = document.getElementById("viewport");
+  if(dns.value!=''){
+    dnsserver = 'https://jtechnologies.github.io/Extended-Internet-Protocol/dns/';
+  } else {
+    dnsserver = dns.value;
+  }
+  var xmlhttp = new XMLHttpRequest();
+           frame.src=myObj;
+         }
+       };
+       xmlhttp.open("GET",dnsserver+url+'.json', true);
+       xmlhttp.send();
+     }
+}
