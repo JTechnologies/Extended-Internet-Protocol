5a736f4155a72e20c8f4fbbe27298a8f1a59bf88
Velocity <me@jtechnologies.xyz>
2020-05-19 02:24:38 +0000

    Get the JS to work.

--- script.js
+++ script.js
@@ -3,20 +3,21 @@ function geturl() {
   var dns = document.getElementById("dns");
   var url = document.getElementById("urlbar").value;
   var frame = document.getElementById("viewport");
-  if(dns.value!=''){
+  if(dns.value==''){
     dnsserver = 'https://jtechnologies.github.io/Extended-Internet-Protocol/dns/';
   } else {
     dnsserver = dns.value;
   }
 var xmlhttp = new XMLHttpRequest();
        xmlhttp.onreadystatechange = function() {
+         console.log(this.status)
          if(this.readyState == 4 && this.status == 200){
            var response = JSON.parse(this.responseText);
            frame.src=response;
            console.log("Request Completed with 200");
        };
+       }
       xmlhttp.open("GET",dnsserver + url + '.json', true);
       xmlhttp.send();
       console.log("Request Sent")
-}
 }
